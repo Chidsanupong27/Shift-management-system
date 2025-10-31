ff8ff9a7a46e0a45ebd4b0d6ac01c194655cac82
Chidsanupong27 <Chidsan7@gmail.com>
2025-10-31 15:51:24 +0700

    Marker, Popup, Tooltip

--- src/components/map/MapView.jsx
+++ src/components/map/MapView.jsx
@@ -1,7 +1,8 @@
 import React from "react";
-import { MapContainer, useMapEvents } from "react-leaflet";
+import { MapContainer, Marker, useMapEvents,Popup, Tooltip } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Layers from "./Layers";
+import useDutyStore from "../../store/useDutyStore";
 
 
 
@@ -19,6 +20,8 @@ const map = useMapEvents({
 
 const MapView = ({adding,onPick}) => {
 
+  const locations = useDutyStore((s)=>s.locations )
+
 
   const center = [13,100]
   return (
@@ -32,6 +35,26 @@ const MapView = ({adding,onPick}) => {
        <Layers/>
 
         <ClickToAdd adding={adding} onPick={onPick}/>
+
+
+        {
+          locations.map((item)=>{
+            return <Marker 
+            key={item.id}
+            position={[item.lat,item.lng]}>
+            <Popup>
+              <div className="text-sm">{item.name}</div>
+              <div className="text-sm text-gray-500">{item.lat},{item.lng}</div>
+            </Popup>
+            <Tooltip direction="center">
+              {item.name}
+            </Tooltip>
+            
+
+            </Marker>
+
+          })
+        }
 
 
 
